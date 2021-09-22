409638d7bfabe5f55c52111f30004b463d918f88
Ritik Ranjan <ritikranjan1920@gmail.com>
2021-09-23 02:48:49 +0530

    Changes

---
+++ zone/static/chart.js
@@ -0,0 +1,24 @@
+
+
+var xValues = [50,60,70,80,90,100,110,120,130,140,150,190,200];
+var yValues = [7,8,8,9,9,9,10,11,14,14,15,89];
+
+new Chart("Chart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      beginAtZero: true
+    }
+  }
+});
